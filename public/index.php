8ec1e69e690e3269530bba24eef9d7e2ded03579
SebastianAvila <jesusaviala@gmail.com>
2022-03-10 13:42:10 -0600

    posible solution

--- public/index.php
+++ public/index.php
@@ -48,8 +48,11 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
+// $response = $kernel->handle(
+//     $request = Request::capture()
+// )->send();
 $response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    $request = App\Custom\Http\Request::capture()
+);
 
 $kernel->terminate($request, $response);
